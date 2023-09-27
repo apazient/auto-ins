e134651b2424c9219e83d6e63415fd25ef2d4425
GKastet <127600582+GKastet@users.noreply.github.com>
2023-09-27 21:28:08 +0200

    loader centered at screen

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,21 @@
 import { BallTriangle } from "react-loader-spinner";
+import { LoaderBox } from "./LoaderStyled";
 
 const Loader = () => {
   return (
-    <BallTriangle
-      height={100}
-      width={100}
-      radius={5}
-      color="#4fa94d"
-      ariaLabel="ball-triangle-loading"
-      wrapperClass={{}}
-      wrapperStyle=""
-      visible={true}
-    />
+    <LoaderBox>
+      <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#4fa94d"
+        ariaLabel="ball-triangle-loading"
+        wrapperClass={{}}
+        wrapperStyle=""
+        visible={true}
+      />
+    </LoaderBox>
   );
 };
 
-export default Loader
+export default Loader;
