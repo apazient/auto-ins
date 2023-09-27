1092c3f660fd1250154b6da9b761403f0c3d4637
apazient <nataliia.amanzhayeva@web.de>
2023-09-27 18:50:19 +0200

    add: sort text in column

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -9,44 +9,68 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { ContainerSection, YellowButton } from "../../style/Global.styled";
-import { InfoSectionContainer } from "./InfoSectionStyle";
+import { InfoSectionContainer, ListText } from "./InfoSectionStyle";
 import { TransitionGroup } from "react-transition-group";
 
 const InfoSection = ({ text }) => {
   const [checked, setChecked] = useState(false);
   const newText = text.split("\n").filter((el) => el !== "");
   const mobText = newText.slice(0, 3);
-  console.log(mobText);
+
   const handleChange = () => {
     console.log(checked);
     setChecked((prev) => !prev);
   };
-  const splitText = (text) => {};
+  const splitText = (text) => {
+    const vieportWidth = window.visualViewport.width;
+    const splitedText = text.split("\n").filter((el) => el !== "");
+    const renderText = [];
+
+    if (vieportWidth < 768) {
+      renderText.push(splitedText.slice(0, 2));
+      renderText.push(splitedText.slice(2));
+      return renderText;
+    }
+    if (vieportWidth >= 768 && vieportWidth < 1080) {
+      renderText.push(splitedText.slice(0, 3));
+      renderText.push(splitedText.slice(3));
+      return renderText;
+    }
+    if (vieportWidth >= 1080) {
+      renderText.push(splitedText.slice(0, 13));
+      renderText.push(splitedText.slice(13));
+      return renderText;
+    }
+  };
+
   return (
-    <>
+    <section>
       <InfoSectionContainer>
-        <List>
-          <List
-            sx={{ display: { sm: "none", md: "none", lg: "none", xl: "none" } }}
-          >
-            {mobText?.map((el, index) => (
-              <Typography variant="body2" key={index}>
+        <Box>
+          <ListText>
+            {splitText(text)[0]?.map((el, index) => (
+              <Typography
+                variant="body2"
+                key={index}
+                sx={{ width: "50%", marginBottom: { sx: "", md: "14px" } }}
+              >
                 {el}
               </Typography>
             ))}
-          </List>
-        </List>
+          </ListText>
+        </Box>
 
         <Fade in={checked} unmountOnExit>
-          <List>
-            {newText?.map((el, index) => (
-              <Typography variant="body2" key={index}>
+          <ListText>
+            {splitText(text)[1]?.map((el, index) => (
+              <Typography variant="body2" key={index} sx={{ width: "50%" }}>
                 {el}
               </Typography>
             ))}
-          </List>
+          </ListText>
         </Fade>
         <FormControlLabel
+          sx={{ display: "block", textAlign: "center" }}
           control={
             <YellowButton onClick={handleChange}>
               {!checked ? "Читати далі" : "Назад"}
@@ -54,7 +78,7 @@ const InfoSection = ({ text }) => {
           }
         />
       </InfoSectionContainer>
-    </>
+    </section>
   );
 };
 
--- src/components/InfoSection/InfoSectionStyle.js
+++ src/components/InfoSection/InfoSectionStyle.js
@@ -1,6 +1,20 @@
+import { List } from "@mui/material";
 import { styled } from "@mui/system";
 import { ContainerSection } from "../../style/Global.styled";
 
 export const InfoSectionContainer = styled(ContainerSection)(({ theme }) => ({
   backgroundColor: theme.palette.primary.white,
+}));
+
+export const ListText = styled(List)(({ theme }) => ({
+  display: "block",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "column",
+    height: "200px",
+  },
+  [theme.breakpoints.up("lg")]: {
+    height: "750px",
+  },
 }));
