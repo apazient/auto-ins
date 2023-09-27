3d4fef04f9995ecd0e556ad8d35d91105265a48d
GKastet <127600582+GKastet@users.noreply.github.com>
2023-09-27 21:38:55 +0200

    header start markup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
-
-
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div>Header</div>
+      <div>
+        <div>
+          <div>logo</div>
+          <div>text</div>
+        </div>
+        <ul>
+          <li>chapter1</li>
+          <li>chapter2</li>
+          <li>chapter3</li>
+        </ul>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
