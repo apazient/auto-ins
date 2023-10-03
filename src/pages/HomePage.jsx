1ad5d6cabfd4889db695deb05ff4c9a8fe3b71bb
apazient <nataliia.amanzhayeva@web.de>
2023-10-03 07:19:27 +0200

    change render position

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,6 @@ import { useEffect } from "react";
 import AdvatagesSection from "../components/AdvatagesSection/AdvatagesSection";
 import { useLocation } from "react-router-dom";
 import Partners from "../components/Partners/Partners";
-import HeroPicture from "../components/HeroPicture/HeroPicture";
 
 const HomePage = () => {
   const { state } = useLocation();
@@ -23,7 +22,7 @@ const HomePage = () => {
   return (
     <>
       <HeroTabs />
-      <HeroPicture />
+
       <AdvatagesSection />
       <CheckInsSection />
       <Partners />
