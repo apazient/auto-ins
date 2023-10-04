e131697f20b21372a8f9762c53ca1808147c81a1
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-10-05 00:12:47 +0300

    ProposalsFilterStyledDone+

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,6 @@ import {
 } from "./HeaderStyled";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
 import { useNavigate } from "react-router-dom";
-import RRR from "../RRR.js/RRR";
 
 const Header = () => {
   const theme = useTheme();
@@ -28,7 +27,6 @@ const Header = () => {
   return (
     <header>
       <HeaderS disableGutters={true} maxWidth={false}>
-        <RRR />
         <HeaderBoxS>
           <LogoBoxS>
             <BoxIconHS className="logoIcon">
