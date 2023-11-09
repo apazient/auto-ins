021ba0c25c17718bc44a0d9ab8ee3f7d3697af9d
apazient <nataliia.amanzhayeva@web.de>
2023-11-09 08:16:58 +0100

    delete react query, mocapi

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,23 @@ import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Loader from "./components/Loader/Loader";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const HomePage = lazy(() => import("./pages/HomePage.jsx"));
 const PricesPage = lazy(() => import("./pages/PricesPage.jsx"));
 const FormPage = lazy(() => import("./pages/FormPage.jsx"));
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: { queries: { refetchOnWindowFocus: false } },
-  });
   return (
-    <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/prices" element={<PricesPage />} />
-            <Route path="/form" element={<FormPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Route>
-        </Routes>
-      </Suspense>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/prices" element={<PricesPage />} />
+          <Route path="/form" element={<FormPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
-import { userNormalize } from "../helpers/dataNormalize/userNormalize";
-
-const baseURL = import.meta.env.VITE_REACT_APP_API_URL;
 const xAuthUser = import.meta.env.VITE_REACT_APP_X_AUTH_USER;
 const xAuthToken = import.meta.env.VITE_REACT_APP_X_AUTH_TOKEN;
 
 export const instance = axios.create({
-  // baseURL: baseURL,
   baseURL: "/api",
 
   headers: {
@@ -17,31 +13,3 @@ export const instance = axios.create({
   },
   mode: "cors",
 });
-
-// export const getUserByEmail = async () => {
-//   const { data } = await instance.get("/user/getByEmail", {
-//     params: { email: "persichek5@gmail.com" },
-//     // params: { email: xAuthUser },
-//   });
-//   return userNormalize(data);
-// };
-
-// export const getCityByName = async (cityName) => {
-//   const { data } = await instance.get("/place", {
-//     params: {
-//       query: cityName,
-//       cdbMtibu: false,
-//       country: "UA",
-//     },
-//   });
-//   return data;
-// };
-// export const getCarModel = async (carModel) => {
-//   const { data } = await instance.get("/auto_model/maker_and_model", {
-//     params: {
-//       query: carModel,
-//     },
-//   });
-
-//   // return data;
-// };
