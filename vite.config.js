7d1c736a48ee64141dd52d13533a59100ea62071
apazient <nataliia.amanzhayeva@web.de>
2023-11-20 11:23:38 +0100

    delete proxy Server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -9,14 +9,14 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   base: "/auto-ins",
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://web.eua.in.ua/eua/api/v15",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-    corse: false,
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://web.eua.in.ua/eua/api/v15",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  //   corse: false,
+  // },
 });
