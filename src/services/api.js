1fe69d7e25ceea36db2b79abcb8f674c6fbbc358
apazient <nataliia.amanzhayeva@web.de>
2023-11-20 09:41:24 +0100

    add key

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const xAuthUser = import.meta.env.VITE_REACT_APP_X_AUTH_USER;
-const xAuthToken = import.meta.env.VITE_REACT_APP_X_AUTH_TOKEN;
+// const xAuthUser = import.meta.env.VITE_REACT_APP_X_AUTH_USER;
+// const xAuthToken = import.meta.env.VITE_REACT_APP_X_AUTH_TOKEN;
+const xAuthUser = "persichek5@gmail.com";
+const xAuthToken = "8a87f6e8-55e5-4448-ba5d-f9466667aca1";
 
 export const instance = axios.create({
   baseURL: "/api",
